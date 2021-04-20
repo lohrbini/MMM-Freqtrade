838c4197737ba21bac011e4a3de47e89762e0255
Julian Lohrber <julian.lohrber@innovo-cloud.de>
2021-04-21 00:25:03 +0200

    switch from ifelse to case; changed notifies; add count

--- MMM-Freqtrade.js
+++ MMM-Freqtrade.js
@@ -6,12 +6,12 @@ Module.register("MMM-Freqtrade", {
 
     defaults: 
     {
-        freqtrade_url: "have you",      // freqtrade ui 'http://my.freqtrade-ui.de'
-        freqtrade_user: "read",         // freqtrade username
-        freqtrade_password: "the",      // freqtrade password
+        freqtrade_url: "",      // freqtrade ui 'http://my.freqtrade-ui.de'
+        freqtrade_user: "r",         // freqtrade username
+        freqtrade_password: "",      // freqtrade password
         freqtrade_category: "",         // which module is getting targeted 'balance, trades, daily, count etc'
-        fetchInterval: 6000,            // query in ms
-        freqtrade_update_token: 7200    // refresh token in ms
+        fetchInterval: 30000,            // query in ms
+        freqtrade_update_token: 120000    // refresh token in ms
     },
 
     start: function ()
@@ -35,34 +35,29 @@ Module.register("MMM-Freqtrade", {
     getDom: function()
         {
             var wrapper = document.createElement("div");
-            var tablerow, tabledata, table, tablecell;
+            var tablerow, tabledata, table;
             wrapper.className = "xsmall"
             
             table = document.createElement("table");
             table.id = "MMM-Freqtrade"
             
-            this.items = [];
-            for (var i = 0; i < jsonData.length; i++)
-            {
-                var object = jsonData[i];
-                for (var property in object)
-                {
-                    this.items.push(object[property]);
-                }
-            }
 
-            // display "maintenance" if there is no data available
-            if (this.items.length === 0 ) 
-            {
-                wrapper.innerHTML = "Awaiting Freqtrade data..";
-                return wrapper;
-            } 
-            // return the formatted data in a table
-            else
-            {
 
-                if (this.config.freqtrade_category === "performance")
-                {
+            console.log("Items: " + this.items);
+           
+            switch (this.config.freqtrade_category)
+            {
+                case "performance":
+                    this.items = [];
+                    for (var i = 0; i < jsonData.length; i++)
+                    {
+                        var object = jsonData[i];
+                        for (var property in object)
+                        {
+                            this.items.push(object[property]);
+                        }
+                    }
+
                     // create headlines
                     var header = table.createTHead();
                     var header_row = header.insertRow(0);
@@ -84,30 +79,71 @@ Module.register("MMM-Freqtrade", {
                         tabledata = tablerow.insertCell();
                         tabledata.appendChild(document.createTextNode(this.items[i]));
                     }
-
                     wrapper.appendChild(table);
-                    return wrapper;
-                }
-                else if (this.config.freqtrade_category === "to_be_done")
-                {
-                    //to be done
-                }
+                    break;
 
+                case "count":
+                    this.items = [];
+                    this.items.push(jsonData);
 
+                    // create headlines
+                    var header = table.createTHead();
+                    var header_row = header.insertRow(0);
+                    var header_topic_0 = header_row.insertCell(0);
+                    var header_topic_1 = header_row.insertCell(0);
+                    var header_topic_2 = header_row.insertCell(0);
+                    header_topic_0.innerHTML = "Current";
+                    header_topic_1.innerHTML = "Max";
+                    header_topic_2.innerHTML = "Total Stake";
+                    table.appendChild(header);
+
+                    for (var i = 0; i <= this.items.length; i++) 
+                    {
+                        if (i % 3 === 0)
+                        {
+                            tablerow = table.insertRow();
+                        }
+                     
+                        tabledata = tablerow.insertCell();
+                        tabledata.appendChild(document.createTextNode(this.items[i]));
+                    }
+                    wrapper.appendChild(table);
+                    break;
+                
+                default:
+                    wrapper.innerHTML = "Awaiting Freqtrade data..";
 
             }
+            return wrapper;          
         },
 
         socketNotificationReceived: function (notification, payload) 
         {
-            if (notification === "MMM-Freqtrade_JSON_DATA") 
-            {
-                Log.info("JSON DATA ARRIVED");
-                jsonData = payload.data;
-                Log.info(jsonData);
-                this.updateDom();
-            }
-    
+            Log.info("JSON DATA ARRIVED");
+            switch (notification)
+              {
+                case "MMM-Freqtrade_JSON_DATA_PERFORMANCE":
+                    jsonData = payload.data;
+                    Log.info("MMM-Freqtrade_JSON_DATA_PERFORMANCE:" + jsonData);
+                    this.updateDom();
+                    break;
+
+                case "MMM-Freqtrade_JSON_DATA_COUNT":
+                    jsonData = [payload.current, payload.max, payload.total_stake];
+                    Log.info("MMM-Freqtrade_JSON_DATA_COUNT:" + jsonData);
+                    this.updateDom();
+                    break;              
+
+                case "MMM-Freqtrade_JSON_DATA_BALANCE":
+                    jsonData = [payload.currency, payload.balance, payload.stake];
+                    Log.info("MMM-Freqtrade_JSON_DATA_BALANCE:" + jsonData);
+                    this.updateDom();                
+                    break;
+
+                default:
+                    jsonData = null;
+                    this.updateDom();                 
+                }
         },
 
         //fetch token from API endpoint
@@ -142,7 +178,32 @@ Module.register("MMM-Freqtrade", {
                 response.json().then((result) => {
                     this.result = result;
                     Log.info(this.name + ": " + "Fetching Data for Category [" + `${this.config.freqtrade_category}` + "]");
-                    this.sendSocketNotification("MMM-Freqtrade_RESULT", {data: this.result});
+                    var switch_me = this.config.freqtrade_category;
+                    switch (switch_me)
+                    {
+                        case "performance":
+                            this.sendSocketNotification("MMM-Freqtrade_RESULT_Performance", {data: this.result});
+                            Log.info("[MMM-Freqtrade] RESULT_Performance:" + this.result);
+                            break;
+
+                        case "count":
+                            this.sendSocketNotification("MMM-Freqtrade_RESULT_Count", {current: (this.result.current).toString() , max: (this.result.max).toString() , stake: (this.result.total_stake).toString()});
+                            Log.info("[MMM-Freqtrade] RESULT_Count:" + "Current" + this.result.current + "|     |" + "Max" + this.result.max + "|     |" + "Stake" + this.result.total_stake);
+                            break;
+
+                        case "balance":
+                            for (var currencies in this.result.currencies)
+                            {
+                                currencies = this.result.currencies[currencies];
+                                this.sendSocketNotification("MMM-Freqtrade_RESULT_Balance", {currency: this.currencies.currency, balance: this.currencies.balance, stake: this.currencies.est_stake});
+                                Log.info("[MMM-Freqtrade] RESULT_Balance:" + "Currency" + this.currencies.currency + "|     |" + "Balance" + this.currencies.balance  + "|     |" + "Stake" + this.currencies.est_stake);
+                            }
+                            break;
+
+                        default:
+                            this.result = null;
+                            this.sendSocketNotification("MMM-Freqtrade_RESULT_Default", {data: this.result});
+                    }
                 });
             });
         }
--- node_helper.js
+++ node_helper.js
@@ -8,13 +8,38 @@ module.exports = NodeHelper.create({
 
 	socketNotificationReceived: function (notification, payload) 
     {
-        console.log("MMM-Freqtrade:"+ " " + "NOTIFICATION ARRIVED");
         var self = this;
+        switch (notification) 
+        {
+            case "MMM-Freqtrade_RESULT_Performance":
+                this.jsonData = payload;
+                this.sendSocketNotification("MMM-Freqtrade_JSON_DATA_PERFORMANCE", this.jsonData);
+                console.log("Performance Data Arrived:" + this.jsonData);
+                break;
+            
+            case "MMM-Freqtrade_RESULT_Count":
+                this.jsonData = [payload.current, payload.max, payload.total_stake];
+                this.sendSocketNotification("MMM-Freqtrade_JSON_DATA_COUNT", this.jsonData);
+                console.log("Count Data Arrived:" + this.jsonData);
+                break;
+            
+            case "MMM-Freqtrade_RESULT_Balance":
+                this.jsonData = [payload.currency, payload.balance, payload.stake];
+                this.sendSocketNotification("MMM-Freqtrade_JSON_DATA_BALANCE", this.jsonData);
+                console.log("Balance Data Arrived:" + this.jsonData);
+                break;
+            
+            default:
+                this.jsonData = ["NO", "DATA", "Available"]
+                this.sendSocketNotification("MMM-Freqtrade_JSON_DATA", this.jsonData);1
+                console.log("Undefined Data Arrived:" + this.jsonData);
+
+        }
         if (notification === "MMM-Freqtrade_RESULT") 
         {
-            this.jsonData = payload;
-            this.sendSocketNotification("MMM-Freqtrade_JSON_DATA", this.jsonData);
-            console.log(this.jsonData);
+            
+            
+            
         }
 
     },
